951593413e95d43497c5eaaa88165fd679650495
unknown <muveitan@gmail.com>
2026-03-22 22:02:37 +0800

    [OnlinePlay][WIP] Added the prototype for player movement in online play settings

---
+++ Assets/Script/NetworkedPlayer/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    private bool ifMovementLocked;
+    private Vector2 currentMoveInput;
+    private void Awake()
+    {
+        if (rb == null) rb = GetComponent<Rigidbody2D>();
+
+        if (animator == null) animator = GetComponent<Animator>();
+
+        if (spriteRenderer == null) spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    public void SetMovementInput(Vector2 moveInput)
+    {
+        if (moveInput.sqrMagnitude > 1f) moveInput = moveInput.normalized;
+
+        currentMoveInput = moveInput;
+    }
+    public void SetMovementLocked(bool ifLocked)
+    {
+        ifMovementLocked = ifLocked;
+
+        if (ifLocked)
+        {
+            currentMoveInput = Vector2.zero;
+            UpdateVisuals(Vector2.zero);
+        }
+    }
+
+    public void Move(float deltaTime)
+    {
+        if (ifMovementLocked) return;
+
+        Vector2 newPosition =  rb.position + currentMoveInput * moveSpeed * deltaTime;
+        rb.MovePosition(newPosition);
+
+        UpdateVisuals(currentMoveInput);
+
+    }
+    public void UpdateVisuals(Vector2 moveInput)
+    {
+        if (animator != null) animator.SetBool("IsMoving", moveInput.sqrMagnitude > 0.001f);
+
+        if (spriteRenderer != null && Mathf.Abs(moveInput.x) > 0.01f) spriteRenderer.flipX = moveInput.x < 0f;
+
+    }
+
+}
---
+++ Assets/Script/Networking/NetworkedPlayerController.cs
@@ -0,0 +1,66 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class NetworkedPlayerController : NetworkBehaviour
+{
+    [Header("Player Movement Reference")]
+    [SerializeField] private PlayerMovement playerMovement;
+
+    private PlayerInputNetworkData currentInputData;
+
+    private void Awake()
+    {
+        if (playerMovement == null) playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (playerMovement == null) playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if (!IsOwner) return;
+
+        PlayerInputNetworkData inputData = ReadLocalInput();
+        playerMovement.UpdateVisuals(inputData.InputDirection);
+        Debug.Log($"[CLIENT {OwnerClientId}] Input read: {inputData.InputDirection}");
+        SubmitInputDataViaServerRpc(inputData);
+    }
+
+    void FixedUpdate()
+    {
+        if (!IsServer) return;
+
+        playerMovement.SetMovementInput(currentInputData.InputDirection);
+        playerMovement.Move(Time.fixedDeltaTime);
+    }
+
+    private PlayerInputNetworkData ReadLocalInput()
+    {
+        PlayerInputNetworkData inputData = new PlayerInputNetworkData();
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector2 inputDirection = new Vector2(horizontalInput, verticalInput);
+        // diagonal input should not be more than the horizontal or the vertical movement
+        if (inputDirection.sqrMagnitude > 1f) inputDirection = inputDirection.normalized;
+
+        inputData.InputDirection = inputDirection;
+        inputData.PrimaryPressed = Input.GetKeyDown(KeyCode.Space);
+        inputData.SecondaryPressed = Input.GetKeyDown(KeyCode.Q);
+        inputData.InteractPressed = Input.GetKeyDown(KeyCode.E);
+
+        return inputData; 
+    }
+
+    [ServerRpc]
+    private void SubmitInputDataViaServerRpc(PlayerInputNetworkData inputData)
+    {
+        if (inputData.InputDirection.sqrMagnitude > 1.0f) inputData.InputDirection = inputData.InputDirection.normalized;
+
+        currentInputData = inputData;
+    }
+}
